d8d721862cd18a3c180a1a1ce67267776e03bb65
Bharathi K <88481319+bharathi6701@users.noreply.github.com>
2024-10-24 23:44:14 +0530

    Add files via upload

---
+++ Project/script.js
@@ -0,0 +1,5 @@
+// JavaScript for adding items to cart (simple console logging example)
+function addToCart(product, price) {
+    console.log(${product} added to cart at $${price});
+    alert(${product} has been added to your cart!);
+}
